bb7df14851e5f94720043308123a58660d36f8d6
Okkotsu <3356142450@qq.com>
2023-08-18 13:00:53 +0800

    用户服务接口

---
+++ services/userService.go
@@ -0,0 +1,77 @@
+package services
+
+import (
+	"errors"
+	message "minitok/proto/pkg"
+	"minitok/repository"
+	"minitok/usal"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func UserRegister(userName, password string) (*message.DouyinUserRegisterResponse, error) {
+	//查询欲注册的用户名是否已经存在
+	err := repository.UserNameIsExist(userName)
+	if err != nil {
+		return nil, err
+	}
+	info, err := repository.InsertUser(userName, password)
+	if err != nil {
+		return nil, err
+	}
+	token, err := usal.GenToken(info.Id, userName)
+	if err != nil {
+		return nil, err
+	}
+	registResponse := &message.DouyinUserRegisterResponse{
+		UserId: info.Id,
+		Token:  token,
+	}
+	return registResponse, nil
+}
+
+func UserLogin(userName, password string) (*message.DouyinUserLoginResponse, error) {
+	info, err := repository.GetUserInfo(userName)
+	if err != nil {
+		return nil, err
+	}
+	//验证密码是否正确
+	err = bcrypt.CompareHashAndPassword([]byte(info.Password), []byte(password))
+	if err != nil {
+		return nil, errors.New("password error")
+	}
+	token, err := usal.GenToken(info.Id, userName)
+	if err != nil {
+		return nil, err
+	}
+	loginResponse := &message.DouyinUserLoginResponse{
+		UserId: info.Id,
+		Token:  token,
+	}
+	return loginResponse, nil
+}
+
+// 获取登录用户的信息
+func UserInfo(userID int64) (*message.DouyinUserResponse, error) {
+	info, err := repository.GetUserInfo(userID)
+	if err != nil {
+		return nil, err
+	}
+	user := messageUserInfo(info)
+	return &message.DouyinUserResponse{User: user}, nil
+}
+
+func messageUserInfo(info repository.User) *message.User {
+	return &message.User{
+		Id:              info.Id,
+		Name:            info.Name,
+		FollowCount:     info.Follow,
+		FollowerCount:   info.Follower,
+		IsFollow:        false,
+		Avatar:          info.Avatar,
+		BackgroundImage: info.BackgroundImage,
+		Signature:       info.Signature,
+		TotalFavorited:  info.TotalFav,
+		FavoriteCount:   info.FavCount,
+	}
+}
