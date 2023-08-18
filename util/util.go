2c4681b8b58bbb078b132755ed1886e92e58b01b
Okkotsu <3356142450@qq.com>
2023-08-18 12:49:22 +0800

    增强工具功能

--- util/util.go
+++ util/util.go
@@ -1,6 +1,10 @@
 package util
 
-import "os"
+import (
+	"math/rand"
+	"os"
+	"time"
+)
 
 // 实现文件夹创建指令
 func Mkdir(path string) error {
@@ -24,4 +28,25 @@ func PathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func GetCurrentTimeForString() string {
+	currentTime := time.Now()
+	return currentTime.Format("200601021504")
+}
+
+func GetCurrentTime() int64 {
+	return time.Now().UnixNano() / 1e6
+}
+
+// 随机生成字符
+func RandomString() string {
+	var letters = []byte("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+	result := make([]byte, 16)
+
+	rand.Seed(time.Now().Unix())
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
 }
